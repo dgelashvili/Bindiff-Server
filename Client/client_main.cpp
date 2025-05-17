8839ff0c47f2c76781122acc93744dbd0500c06b
TamarZerekidze <zerekidzetamar03@gmail.com>
2025-05-17 20:03:42 +0400

    BinDiff-3: Removed 'main.cpp' and divided it into 'server_main.cpp' and 'client_main.cpp'. Added configurations for both executables, so it can mimic two processes independently.

--- Client/client_main.cpp
+++ Client/client_main.cpp
@@ -1,8 +1,7 @@
 #include <iostream>
-#include "Adder.h"
+#include <Adder.h>
 
 int main() {
-	std::cout << "Hello, BinDiff project!\n";
 	Adder adder = Adder(2, 3);
 	std::cout << adder.add() << std::endl;
 	return 0;
