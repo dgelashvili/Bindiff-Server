73ed8dec4b2ff86cb3d9beb2f6b309d7d0212913
TamarZerekidze <zerekidzetamar03@gmail.com>
2025-05-18 19:28:54 +0400

    BinDiff-5: created client stub instance for simple communication. connected client to the channel and implemented 'sayHello' wrapper to send request and receive reply message.

--- Client/client_main.cpp
+++ Client/client_main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-#include <Adder.h>
+#include <grpcpp/grpcpp.h>
+#include <helloworld.grpc.pb.h>
+
+class HelloWorldClient {
+	private:
+		std::unique_ptr<helloworld::Greeter::Stub> stub_;
+	public:
+		explicit HelloWorldClient(const std::shared_ptr<grpc::Channel>& channel): stub_(helloworld::Greeter::NewStub(channel)) {};
+
+		void SayHello() const {
+			grpc::ClientContext context;
+			helloworld::HelloRequest request;
+			helloworld::HelloReply reply;
+			request.set_name("Takusi");
+			if (const grpc::Status status = stub_->SayHello(&context, request, &reply); status.ok()) {
+				std::cout << reply.message() << std::endl;
+			} else {
+				std::cout << status.error_code() << ": " << status.error_message() << std::endl;
+			}
+		}
+};
 
 int main() {
-	Adder adder = Adder(2, 3);
-	std::cout << adder.add() << std::endl;
+	const HelloWorldClient client(grpc::CreateChannel("localhost:50051", grpc::InsecureChannelCredentials()));
+	client.SayHello();
 	return 0;
 }
