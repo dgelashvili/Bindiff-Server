02816798d1a5d59ed255885917d5568fd2518d47
dgelashvili <demetre@LAPTOP-PCEQABLP>
2025-05-18 22:06:54 +0400

    BinDiff-5: Added 'HelloWorldServer' class which is Service class implementing 'SayHello' method.

--- Server/server_main.cpp
+++ Server/server_main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <grpcpp/grpcpp.h>
+#include <helloworld.grpc.pb.h>
+
+class HelloWorldServer final : public helloworld::Greeter::Service {
+	::grpc::Status SayHello(
+		grpc::ServerContext *context,
+		const helloworld::HelloRequest *request,
+		helloworld::HelloReply *response) override {
+
+		response->set_message("Demetre " + request->name());
+		return ::grpc::Status::OK;
+	}
+};
 
 int main() {
-	std::cout << "Hello, BinDiff project!\n";
+	const std::string address = "localhost:50051";
+	grpc::ServerBuilder builder;
+	builder.AddListeningPort(address, grpc::InsecureServerCredentials());
+
+	HelloWorldServer service;
+	builder.RegisterService(&service);
+
+	const std::unique_ptr<grpc::Server> server = builder.BuildAndStart();
+
+	std::cout << "Server listening on " << address << std::endl;
+
+	server->Wait();
 	return 0;
 }
