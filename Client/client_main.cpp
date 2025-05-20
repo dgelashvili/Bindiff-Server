31591107404f9cdc6ccb74ac2d999c17ec427c5c
dgelashvili <demetre@LAPTOP-PCEQABLP>
2025-05-20 17:22:20 +0400

    Revert "BinDiff-6: No idea what is goint wrong"
    This reverts commit ca65abbf10c79348e61ccf8a2626565981012842.

--- Client/client_main.cpp
+++ Client/client_main.cpp
@@ -1,29 +1,20 @@
 #include <iostream>
-#include <fstream>
 #include <grpcpp/grpcpp.h>
-#include <bindiff.grpc.pb.h>
+#include <helloworld.grpc.pb.h>
 
 class HelloWorldClient {
 	private:
-		std::unique_ptr<bindiff::BinExportService::Stub> stub_;
+		std::unique_ptr<helloworld::Greeter::Stub> stub_;
 	public:
-		explicit HelloWorldClient(const std::shared_ptr<grpc::Channel>& channel): stub_(bindiff::BinExportService::NewStub(channel)) {};
+		explicit HelloWorldClient(const std::shared_ptr<grpc::Channel>& channel): stub_(helloworld::Greeter::NewStub(channel)) {};
 
 		void SayHello() const {
 			grpc::ClientContext context;
-
-			std::ifstream file("/home/demetre/Bindiff-Server/Client/test_binDiff_1.exe.BinExport", std::ios::binary);
-			std::string file_data((std::istreambuf_iterator<char>(file)),
-								  std::istreambuf_iterator<char>());
-
-			bindiff::BinExportRequest request;
-			bindiff::BinExportResponse reply;
-			request.set_binexport_data(file_data);
-
-			if (const grpc::Status status = stub_->UploadBinExport(&context, request, &reply); status.ok()) {
-				for (const auto& elem : reply.function_names()) {
-					std::cout << elem << std::endl;
-				}
+			helloworld::HelloRequest request;
+			helloworld::HelloReply reply;
+			request.set_name("Takusi");
+			if (const grpc::Status status = stub_->SayHello(&context, request, &reply); status.ok()) {
+				std::cout << reply.message() << std::endl;
 			} else {
 				std::cout << status.error_code() << ": " << status.error_message() << std::endl;
 			}
--- Server/server_main.cpp
+++ Server/server_main.cpp
@@ -1,25 +1,15 @@
 #include <iostream>
 #include <grpcpp/grpcpp.h>
-#include <binexport2.grpc.pb.h>
-#include <bindiff.grpc.pb.h>
+#include <helloworld.grpc.pb.h>
 
-class HelloWorldServer final : public bindiff::BinExportService::Service {
-	grpc::Status UploadBinExport(
+class HelloWorldServer final : public helloworld::Greeter::Service {
+	::grpc::Status SayHello(
 		grpc::ServerContext *context,
-		const bindiff::BinExportRequest *request,
-		bindiff::BinExportResponse *response) override {
+		const helloworld::HelloRequest *request,
+		helloworld::HelloReply *response) override {
 
-		BinExport2::CallGraph call_graph;
-		if (!call_graph.ParseFromString(request->binexport_data())) {
-			return {grpc::StatusCode::INVALID_ARGUMENT, "Failed to parse BinExport data"};
-		}
-
-		std::cout << call_graph.DebugString() << std::endl;
-		for (const auto& function : call_graph.vertex()) {
-			response->add_function_names(std::to_string(function.address()));
-		}
-
-		return grpc::Status::OK;
+		response->set_message("Demetre " + request->name());
+		return ::grpc::Status::OK;
 	}
 };
 
