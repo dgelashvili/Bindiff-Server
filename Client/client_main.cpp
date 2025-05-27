db8bdf3c5eca21d41d865dbad04224a96814bc98
dgelashvili <demetre@LAPTOP-PCEQABLP>
2025-05-27 22:30:14 +0400

    ServerCache-9: Removed all unnecessary classes which where present in 'client_main.cpp' and 'server_main.cpp'

--- Client/client_main.cpp
+++ Client/client_main.cpp
@@ -1,35 +1,8 @@
 #include <iostream>
 #include <fstream>
+
 #include "grpcpp/grpcpp.h"
-#include "binexportParser.grpc.pb.h"
 #include "bin_diff.grpc.pb.h"
-
-class BinexportParserClient {
-private:
-	std::unique_ptr<binexportParser::Parser::Stub> stub_;
-public:
-	explicit BinexportParserClient(const std::shared_ptr<grpc::Channel>& channel): stub_(binexportParser::Parser::NewStub(channel)) {};
-
-	void Parse() const {
-		grpc::ClientContext context;
-
-		std::ifstream file("../Client/test_binDiff_1.exe.BinExport", std::ios::binary);
-		std::string file_data((std::istreambuf_iterator<char>(file)),
-							  std::istreambuf_iterator<char>());
-
-		binexportParser::ParseRequest request;
-		binexportParser::ParseReply reply;
-		request.set_content(file_data);
-
-		if (const grpc::Status status = stub_->Parse(&context, request, &reply); status.ok()) {
-			for (const auto& elem : reply.names()) {
-				std::cout << elem << std::endl;
-			}
-		} else {
-			std::cout << status.error_code() << ": " << status.error_message() << std::endl;
-		}
-	}
-};
 
 class BinDiffClient {
 public:
--- Server/server_main.cpp
+++ Server/server_main.cpp
@@ -1,31 +1,5 @@
-#include <iostream>
-#include <sha256Wrapper.h>
-
 #include "grpcpp/grpcpp.h"
-#include "binexport2.pb.h"
-#include "binexportParser.grpc.pb.h"
 #include "BinDiffServer.h"
-
-class BinexportParserServer final : public binexportParser::Parser::Service {
-	::grpc::Status Parse(
-		grpc::ServerContext *context,
-		const binexportParser::ParseRequest *request,
-		binexportParser::ParseReply *response) override {
-
-		BinExport2 binexport;
-
-		if (!binexport.ParseFromString(request->content())) {
-			std::cerr << "Failed to parse BinExport file" << std::endl;
-			return {grpc::StatusCode::INVALID_ARGUMENT, "Failed to parse BinExport data"};
-		}
-
-		for (const auto& function : binexport.call_graph().vertex()) {
-			response->add_names(sha_256(function.mangled_name()));
-		}
-
-		return ::grpc::Status::OK;
-	}
-};
 
 void BuildAndStartServer(const std::string& server_address) {
 	grpc::ServerBuilder builder;
