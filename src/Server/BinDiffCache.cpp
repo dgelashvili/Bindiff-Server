37c227641f474b05e7b45df9441ca21c8668b1e5
dgelashvili <demetre@LAPTOP-PCEQABLP>
2025-05-28 22:54:59 +0400

    ServerCache-15: Made 'BinDiffCache' class thread-safe

--- src/Server/BinDiffCache.cpp
+++ src/Server/BinDiffCache.cpp
@@ -1,17 +1,25 @@
 #include "BinDiffCache.h"
 
 void BinDiffCache::add(const std::string &id, const std::shared_ptr<BinExportContent> &content) {
-	if (contains(id)) return;
+	std::lock_guard lock(cache_mutex_);
+	if (contains_without_lock(id)) return;
 	cache_[id] = content;
 }
 
 std::shared_ptr<BinExportContent> BinDiffCache::get(const std::string &id) {
-	if (!contains(id)) {
+	std::lock_guard lock(cache_mutex_);
+	if (!contains_without_lock(id)) {
 		throw std::runtime_error("No parsed file with given ID");
 	}
 	return cache_[id];
 }
 
 bool BinDiffCache::contains(const std::string &id) {
+	std::lock_guard lock(cache_mutex_);
+	return contains_without_lock(id);
+}
+
+bool BinDiffCache::contains_without_lock(const std::string &id) {
 	return cache_.find(id) != cache_.end();
 }
+
