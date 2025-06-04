0b671068d1d66a51ad69ba0174b9eb7fa15011fc
TamarZerekidze <zerekidzetamar03@gmail.com>
2025-06-04 23:24:11 +0400

    DiffingEngine-5: Added 'function_block_counts_' and 'function_instruction_counts_' private fields inside 'BinExportContent' class for future matching algorithms.

--- src/Server/BinExportContent.cpp
+++ src/Server/BinExportContent.cpp
@@ -5,9 +5,38 @@ BinExportContent::BinExportContent(const std::string &file_bytes) {
 		throw std::runtime_error("Failed to parse BinExport file");
 	}
 
+	fill_flow_graph_address_map();
+
 	for (const auto& func : binexport_raw_.call_graph().vertex()) {
 		function_names_.push_back(func.mangled_name());
 		function_addresses_.push_back(func.address());
+
+		if (flow_graph_address_map_.count(func.address())) {
+			const auto& flow_graph = flow_graph_address_map_[func.address()];
+			function_block_counts_.push_back(flow_graph->basic_block_index_size());
+
+			int instruction_count = 0;
+			for (const auto& block_index : flow_graph->basic_block_index()) {
+				const auto& block = binexport_raw_.basic_block()[block_index];
+
+				for (const auto& range : block.instruction_index()) {
+					if (!range.has_begin_index()) continue;
+					const int begin = range.begin_index();
+					const int end = range.has_end_index() ? range.end_index() : begin + 1;
+					instruction_count += (end - begin);
+				}
+			}
+			function_instruction_counts_.push_back(instruction_count);
+		} else {
+			function_block_counts_.push_back(0);
+			function_instruction_counts_.push_back(0);
+		}
+	}
+
+	for (int i = 0; i < function_addresses_.size(); i++) {
+		std::cout << function_names_[i] << " "
+			<< function_block_counts_[i] << " "
+			<< function_instruction_counts_[i] << std::endl;
 	}
 }
 
@@ -15,10 +44,38 @@ const BinExport2 &BinExportContent::get_raw() {
 	return binexport_raw_;
 }
 
-std::vector<std::string> BinExportContent::get_function_names() const {
+const std::vector<std::string>& BinExportContent::get_function_names() const {
 	return function_names_;
 }
 
-std::vector<std::uint64_t> BinExportContent::get_function_addresses() const {
+const std::vector<std::uint64_t>& BinExportContent::get_function_addresses() const {
 	return function_addresses_;
+}
+
+const std::vector<int>& BinExportContent::get_function_block_counts() const {
+	return function_block_counts_;
+}
+
+const std::vector<int>& BinExportContent::get_function_instruction_counts() const {
+	return function_instruction_counts_;
+}
+
+void BinExportContent::fill_flow_graph_address_map() {
+	for (auto& flow_graph : binexport_raw_.flow_graph()) {
+		if (!flow_graph.has_entry_basic_block_index()) continue;
+
+		const int entry_block_index = flow_graph.entry_basic_block_index();
+
+		if (entry_block_index >= binexport_raw_.basic_block_size()) continue;
+		const auto& entry_block = binexport_raw_.basic_block()[entry_block_index];
+
+		if (entry_block.instruction_index_size() == 0) continue;
+		const auto& first_range = entry_block.instruction_index(0);
+
+		const int instr_index = first_range.begin_index();
+		if (instr_index >= binexport_raw_.instruction_size()) continue;
+
+		const auto& instr = binexport_raw_.instruction(instr_index);
+		flow_graph_address_map_[instr.address()] = &flow_graph;
+	}
 }
--- src/Server/BinExportContent.h
+++ src/Server/BinExportContent.h
@@ -11,13 +11,21 @@ public:
 	explicit BinExportContent(const std::string& file_bytes);
 
 	const BinExport2& get_raw();
-	std::vector<std::string> get_function_names() const;
-	std::vector<std::uint64_t> get_function_addresses() const;
+	const std::vector<std::string>& get_function_names() const;
+	const std::vector<std::uint64_t>& get_function_addresses() const;
+	const std::vector<int>& get_function_block_counts() const;
+	const std::vector<int>& get_function_instruction_counts() const;
 
 private:
+	void fill_flow_graph_address_map();
+private:
 	BinExport2 binexport_raw_;
+	std::unordered_map<uint64_t, const BinExport2_FlowGraph*> flow_graph_address_map_;
+
 	std::vector<std::string> function_names_;
 	std::vector<std::uint64_t> function_addresses_;
+	std::vector<int> function_block_counts_;
+	std::vector<int> function_instruction_counts_;
 };
 
 #endif //BINDIFFCONTENT_H
