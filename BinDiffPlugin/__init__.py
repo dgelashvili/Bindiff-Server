690ffdc9308e929b18e61f2c870bf752fa92aa6d
dgelashvili <demetre@LAPTOP-PCEQABLP.localdomain>
2025-07-08 23:39:10 +0400

    BinDiffPlugin-2: Read Binary Ninja plugin API and wrote very simple BinDiffPlugin which is responsible for 'Uploading' files. It just keeps user uploaded files in the in-memory directory and if user wants to view functions of certain .BinExport file, the plugin checks whether the file is present in the directory. If not, shows error message box

---
+++ BinDiffPlugin/__init__.py
@@ -0,0 +1,54 @@
+import os
+import sys
+
+from binaryninja import log_info, log_error, show_message_box
+from binaryninja.plugin import PluginCommand
+
+plugin_dir = os.path.dirname(os.path.abspath(__file__))
+if plugin_dir not in sys.path:
+    sys.path.insert(0, plugin_dir)
+
+try:
+    from Generated import bin_diff_pb2, bin_diff_pb2_grpc
+
+    IMPORTED = True
+except ImportError as e:
+    IMPORTED = False
+    log_error(f"Could not import all the necessary modules: {e}")
+
+
+class BinDiffPlugin:
+    def __init__(self):
+        self.server_address = "localhost:50051"
+        self.uploaded_files = {}
+
+    def upload_current_file(self, bv):
+        file_id = 1
+
+        if file_id:
+            self.uploaded_files[bv.file.filename] = file_id
+
+    def view_functions(self, bv):
+        path = bv.file.filename
+        if self.uploaded_files.__contains__(path):
+            return
+        else:
+            show_message_box("Error", "This file has not been uploaded to the server")
+
+
+if IMPORTED:
+    plugin = BinDiffPlugin()
+
+    PluginCommand.register(
+        "BinDiffPlugin\\Upload Current File",
+        "Upload .BinExport for currently open .exe file",
+        plugin.upload_current_file
+    )
+
+    PluginCommand.register(
+        "BinDiffPlugin\\View Functions",
+        "View functions from uploaded .BinExport of currently open .exe file",
+        plugin.view_functions
+    )
+
+    log_info("BinDiffPlugin loaded - 2 commands available")
